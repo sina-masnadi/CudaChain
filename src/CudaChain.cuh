4d833ba24e5d283fc9998ebed97847357b2cb2fc
Sina <sina.masnadi@gmail.com>
2020-04-20 09:58:25 -0400

    Changed cuda and added CudaChain namespace

--- src/CudaChain.cuh
+++ src/CudaChain.cuh
@@ -24,17 +24,19 @@ using namespace std;
 #define BLOCK_SIZE 1024
 #define CudaChainPoint  float2
 
-void importQHull(thrust::host_vector<float> &pts_x,
-                 thrust::host_vector<float> &pts_y,
-                 std::string &path);
+namespace CudaChain {
+    void importQHull(thrust::host_vector<float> &pts_x,
+                     thrust::host_vector<float> &pts_y,
+                     std::string &path);
 
-int cudaChainNew(const thrust::host_vector<float> &x,
-                 const thrust::host_vector<float> &y,
-                 thrust::host_vector<float> &hull_x,
-                 thrust::host_vector<float> &hull_y);
+    int cudaChainNew(const thrust::host_vector<float> &x,
+                     const thrust::host_vector<float> &y,
+                     thrust::host_vector<float> &hull_x,
+                     thrust::host_vector<float> &hull_y);
 
-int simpleHull_2D(CudaChainPoint *V, int n, CudaChainPoint *H);
+    int simpleHull_2D(CudaChainPoint *V, int n, CudaChainPoint *H);
 
-void exportOFF(CudaChainPoint *pts, int n);
+    void exportOFF(CudaChainPoint *pts, int n);
+}
 
 #endif //CUDACHAIN_LIBRARY_CUH
